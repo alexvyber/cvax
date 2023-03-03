13c1d219363c61df585446ee2a4acdcf8fffbc28
Alexey Sokolov <alexvyber@gmail.com>
2023-03-03 23:39:54 +0300

    remove .config

--- .config/lint-staged.config.mjs
+++
@@ -1,6 +0,0 @@
-export default {
-  "*": filenames =>
-    `pnpm run prettier -- --write ${filenames.map(filename => `'${filename}'`).join(" ")}`,
-  "**/*.{js,jsx,ts,tsx}": () => "pnpm tsc",
-  "**/package.json": () => "pnpm lint:deps",
-}
