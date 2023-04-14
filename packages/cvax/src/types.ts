a101cbe18b543c91f5f2ec4a06a363a14965f8e7
Alexey Sokolov <alexvyber@gmail.com>
2023-04-14 12:17:58 +0300

    fix type error

--- packages/cvax/src/types.ts
+++ packages/cvax/src/types.ts
@@ -1,6 +1,6 @@
 export type ClassPropKey = "className"
 export type ClassValue = string | null | undefined | readonly ClassValue[]
-export type ClassProp = { className: ClassValue } | { className?: never }
+export type ClassProp = { className?: ClassValue } | { className?: never }
 export type OmitUndefined<T> = T extends undefined ? never : T
 export type StringToBoolean<T> = T extends "true" | "false" ? boolean : T
 export type CxOptions = ClassValue[]
