f15cac5b05e73974084278771004e96d1917016b
Alexey Sokolov <alexvyber@gmail.com>
2023-06-08 21:20:13 +0300

    update packages and configs

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,18 +1,18 @@
 module.exports = {
-  printWidth: 100,
-  semi: false,
-  arrowParens: "avoid",
-  tabWidth: 2,
-  useTabs: false,
-  trailingComma: "all",
-  singleQuote: false,
-  bracketSameLine: true,
+	printWidth: 100,
+	semi: false,
+	arrowParens: "avoid",
+	tabWidth: 2,
+	useTabs: false,
+	trailingComma: "all",
+	singleQuote: false,
+	bracketSameLine: true,
 
-  plugins: [
-    require.resolve("prettier-plugin-astro"),
-    require.resolve("prettier-plugin-packagejson"),
-    require.resolve("prettier-plugin-tailwindcss"),
-  ],
+	plugins: [
+		require.resolve("prettier-plugin-astro"),
+		require.resolve("prettier-plugin-packagejson"),
+		require.resolve("prettier-plugin-tailwindcss"),
+	],
 
-  pluginSearchDirs: ["."],
-}
+	pluginSearchDirs: ["."],
+};
