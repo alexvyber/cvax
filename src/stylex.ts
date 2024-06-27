49639ccbfe3b59268ef8270b27899226af30544e
Alexey Sokolov <alexvyber@gmail.com>
2024-06-28 00:38:24 +0300

    stylex wipg

--- src/stylex.ts
+++ src/stylex.ts
@@ -1,5 +1,6 @@
 import type { StringToBoolean } from "."
 import type { UserAuthoredStyles } from "@stylexjs/stylex/lib/StyleXTypes"
+import * as stylex from "@stylexjs/stylex"
 
 type CvaxStyles = { styles?: UserAuthoredStyles }
 
@@ -9,14 +10,15 @@ type Cvax = <_ extends "iternal use only", V extends CvaxVariantShape>(config: {
   compoundVariants?: (V extends CvaxVariantShape
     ? (
         | CvaxVariantSchema<V>
-        | {
-            [Variant in keyof V]?: StringToBoolean<keyof V[Variant]> | StringToBoolean<keyof V[Variant]>[] | undefined
-          }
+        | { [Variant in keyof V]?: StringToBoolean<keyof V[Variant]> | StringToBoolean<keyof V[Variant]>[] | undefined }
       ) &
         CvaxStyles
     : CvaxStyles)[]
   defaultVariants?: CvaxVariantSchema<V>
-}) => (props?: V extends CvaxVariantShape ? CvaxVariantSchema<V> & CvaxStyles : CvaxStyles) => string
+}) => (props?: V extends CvaxVariantShape ? CvaxVariantSchema<V> & CvaxStyles : CvaxStyles) => Readonly<{
+  className?: string
+  style?: Readonly<{ [key: string]: string | number }>
+}>
 
 type CvaxVariantSchema<V extends CvaxVariantShape> = {
   [Variant in keyof V]?: StringToBoolean<keyof V[Variant]> | undefined | "unset"
@@ -26,8 +28,30 @@ type CvaxVariantShape = {
   [config: string]: { [variant: string]: UserAuthoredStyles }
 }
 
-const cvax: Cvax = (_) => {
+const cvax: Cvax = (config) => {
+  if (!config) {
+    return (props: UserAuthoredStyles) => stylex.props(stylex.create({ props }).props)
+  }
+
   return null as any
 }
 
 export { cvax }
+
+const styles = stylex.create({
+  main: {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    justifyContent: "center",
+    display: "flex",
+    alignItems: "center",
+  },
+})
+
+stylex.props(styles.main)
+stylex.props(styles.card)
